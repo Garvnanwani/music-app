abaee2b4529db6b6c5b5b6c18000a43982ef80bd
Garv nanwani <garvwashere@gmail.com>
2021-08-26 08:34:50 +0530

    welcome component setup

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+const Welcome = () => {
+    return (
+        <div>
+            <div>
+                {/* <Link to="/"> */}
+                <img src="" alt="musica logo" />
+                {/* </Link> */}
+                <div>
+                    <div>
+                        <h3>What&apos;s Your Mood</h3>
+                    </div>
+                    <div>
+                        <p>whatever you want!!</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="music vector" />
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
